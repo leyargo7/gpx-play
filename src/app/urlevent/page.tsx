f20f88714b8f04fbc70da90db2ab0baa4af9c0e1
Arley Gonzalez <leyargo@hotmail.com>
2024-04-17 20:51:50 -0500

    update correcion archivo urlevent a produccion

--- src/app/urlevent/page.tsx
+++ src/app/urlevent/page.tsx
@@ -1,36 +1,37 @@
-// "use client"
-// import axios from "axios"
-// import { useEffect, useState } from "react"
+//"use client"
+//import axios from "axios"
+//import { useEffect, useState } from "react"
 
 // async function getEvent() {
 //     const res = await axios.get('http://localhost:3000/api/sandboxevent')
 //     return res.data
 // }
 
-// const UrlEvent = () => {
-//     const [event, setEvent] = useState(null)
+const UrlEvent = () => {
+    // const [event, setEvent] = useState(null)
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const result = await getEvent()
-//             setEvent(result)
-//         }
-//         fetchData()
-//     }, [])
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const result = await getEvent()
+    //         setEvent(result)
+    //     }
+    //     fetchData()
+    // }, [])
 
-//     if (!event) {
-//         return <div>Loading...</div>
-//     }
+    // if (!event) {
+    //     return <div>Loading...</div>
+    // }
 
     
 
-//   return (
-//     <div>
-//         {
-//             JSON.stringify(event)
-//         }
-//     </div>
-//   )
-// }
+  return (
+    <div>
+        {/* {
+            JSON.stringify(event)
+        } */}
+        <h1>processing...</h1>
+    </div>
+  )
+}
 
-// export default UrlEvent
+export default UrlEvent
