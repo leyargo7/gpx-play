4fb37ec2296840014b84ac4a550b9bc12383fefc
Arley Gonzalez <leyargo@hotmail.com>
2024-04-18 07:07:47 -0500

    update correcion archivo playlist17

--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -13,44 +13,47 @@ interface Video {
 //const { NEXT_PUBLIC_BACKEND_URL } = process.env
 
 
-const getVideos = async () => {
+// const getVideos = async () => {
  
-  try {
-    //const response = await axios.get('http://localhost:3000/api/filesServer')
-    const response = await axios.get('https://gpx-play.vercel.app/api/filesServer')
-    return response.data
-  } catch (error) {
-    console.error(error)
-    return error
+//   try {
+//     //const response = await axios.get('http://localhost:3000/api/filesServer')
+//     const response = await axios.get('https://gpx-play.vercel.app/api/videos')
+//     const data = response.data
+//     console.log(data)
+//     return data
+//   } catch (error) {
+//     console.error(error)
+//     return error
     
-  }
-}
+//   }
+// }
 
 async function MainVideos() {
-  const dbVideos = await getVideos()
-  console.log(dbVideos)
+  // const dbVideos = await getVideos()
+  // console.log(dbVideos)
   
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 justify-center p-4">
-      {dbVideos.map((video: Video) => (
-        <Link href={`/playlist/${video._id}`} key={video._id}>
-          <div className="bg-purple-500 py-3 cursor-pointer rounded-md hover:bg-yellow-400 w-full h-full flex  flex-col justify-between">
-            <h1 className='font-bold text-lg mb-2'>{video.title}</h1>
-            <div>
-            <Image
-              src={video.thumbnailUrl}
-              alt={video.title}
-              priority={true}
-              width={300}
-              height={300}
-              className="object-cover w-full"
-            />
-            </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+    // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 justify-center p-4">
+    //   {dbVideos.map((video: Video) => (
+    //     <Link href={`/playlist/${video._id}`} key={video._id}>
+    //       <div className="bg-purple-500 py-3 cursor-pointer rounded-md hover:bg-yellow-400 w-full h-full flex  flex-col justify-between">
+    //         <h1 className='font-bold text-lg mb-2'>{video.title}</h1>
+    //         <div>
+    //         <Image
+    //           src={video.thumbnailUrl}
+    //           alt={video.title}
+    //           priority={true}
+    //           width={300}
+    //           height={300}
+    //           className="object-cover w-full"
+    //         />
+    //         </div>
+    //       </div>
+    //     </Link>
+    //   ))}
+    // </div>
+    <div>hola</div>
   )
 }
 
